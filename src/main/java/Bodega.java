0fb34560f53bf394434f762a7eb602fff9e4a3ea
jhojanGilRojas <111826401+jhojanGilRojas@users.noreply.github.com>
2022-11-02 00:26:29 -0500

    Update Bodega.java

--- src/main/java/Bodega.java
+++ src/main/java/Bodega.java
@@ -1,6 +1,5 @@
 public class Bodega extends Propiedad{
     String zona;
-    String nombre;
     public Bodega(String dirrecion, double valor, float area, Propietario propietario, String zona) {
         super(dirrecion, valor, area, propietario);
         this.zona = zona;
