1ead36dd6f0acc92b6afe0d6a78d0ad57801261a
AlejaGuzmanRios <111519886+AlejaGuzmanRios@users.noreply.github.com>
2022-11-02 01:36:09 -0500

    cambio

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -8,6 +8,8 @@ public class Cliente extends Usuario {
 
     public List <Propiedad>  buscarPropiedad(Propiedad propiedad){
 
+        //Esta clase segun eso es facil, es un filtro que se aplica la lista de propiedades que esta en finca raiz
+        //para hacer una nueva lista de las propiedades con el tipo de propiedad que vengan por parametro
         return null;
     }
     public void alquilar(){
