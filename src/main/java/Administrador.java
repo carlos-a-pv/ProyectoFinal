b96a98b32ce3c2dfb3c9117e37dcf202551af62e
carlos-a-pv <64906698+carlos-a-pv@users.noreply.github.com>
2022-11-03 14:08:26 -0500

    fixed conflicts

--- src/main/java/Administrador.java
+++ src/main/java/Administrador.java
@@ -7,23 +7,4 @@ public class Administrador extends Usuario {
         super(nombre, userId, password,cargo);
     }
 
-<<<<<<< HEAD
-    public void registrarEmpleado(Empleado empleado, Exception e){
-        
-    }
-    public void visualizarReportes (DateFormat periodo, Exception e){
-
-
-    }
-    public void bloquearCuenta(){
-
-
-    }
-    public void actualizarDatosEmpleado("Carlos", "1234", "1234"){
-
-
-    }
-=======
-
->>>>>>> 519a96d9ad0c6883cc423b3ebdb77f4354fd3ac0
 }
